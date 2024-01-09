d2215300f1199d2e50f1620c92ff5cada3b7aef4
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2024-01-09 05:34:14 +0000

    just completed basic 3 js questions

---
+++ week1.2/1.2.1.js
@@ -0,0 +1,24 @@
+//1.1 : greeting a person with first name and last name
+// var fname="dibya"
+// var lname="sahoo"
+// console.log("hello "+"fname"+"lname"+" happy to see you")
+
+//1.2 greets a person based on their gender (ifelse)
+
+// var gender="female"
+
+// if(gender==="male"){
+//     console.log("hello Sir")
+// }
+// else if(gender==="female"){
+//     console.log("hello mam")
+// }
+// else if(gender==="other"){
+//     console.log("hello")
+// }
+
+//1.3 counts from 0 to 1000 and prints it using for loop
+
+// for(let i=0;i<=1000;i++){
+//     console.log(i+" ")
+// }
